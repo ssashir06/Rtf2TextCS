e1a4623f6b136bc1512689a56b8e5246711bdebf
Hiroshi Takeda <takejaponpon+git@gmail.com>
2014-03-22 16:26:39 +0900

    First commit, with solution and readme.

---
+++ README.md
@@ -0,0 +1,2 @@
+This project simply read RTF file to convert to plain text.
+See http://msdn.microsoft.com/en-us/library/cc488002.aspx
---
+++ RtfToText/RtfToText/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RtfToText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() != 1) return;
+
+            string filename = args[0];
+            if (!System.IO.File.Exists(filename)) return;
+
+            string rtf_content;
+            try
+            {
+                rtf_content = System.IO.File.ReadAllText(filename);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.Write(ex.Message);
+                return;
+            }
+
+            Console.Write(ConvertToPlainText(rtf_content));
+        }
+
+        static string ConvertToPlainText(string rtf_content)
+        {
+            RichTextBox rt = new RichTextBox
+            {
+                Rtf = rtf_content,
+            };
+            return rt.Text;
+        }
+    }
+}
